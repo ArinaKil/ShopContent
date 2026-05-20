1f42d3bb575eef70684636e1ab3e8f2674fbe5d4
axxuetam <axxuetam@gmail.com>
2026-05-20 11:46:56 +0500

    Разработанные классы общего пользования. kilunina

---
+++ ShopContent/Classes/Connection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ShopContent.Classes
+{
+    public class Connection
+    {
+        private static readonly string config = "server=student.permaviat.ru;Trusted_Connection=No;DataBase=ShopContent;User=;PWD=;";
+        public static SqlConnection OpenConnection()
+        {
+            SqlConnection connection = new SqlConnection(config);
+            connection.Open();
+            return connection;
+        }
+        public static SqlDataReader Query(string SQL, SqlConnection connection)
+        {
+            SqlCommand command = new SqlCommand(SQL, connection);
+            return command.ExecuteReader();
+        }
+        public static void CloseConnection(SqlConnection connection)
+        {
+            connection.Close();
+            SqlConnection.ClearPool(connection);
+        }
+    }
+}
